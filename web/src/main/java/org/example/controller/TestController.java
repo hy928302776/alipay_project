ed927e2e9274c00429889ef7dab85fd02a309e52
hy928302776 <hy928302776@github.com>
2023-06-01 16:57:16 +0800

    f

--- web/src/main/java/org/example/controller/TestController.java
+++ web/src/main/java/org/example/controller/TestController.java
@@ -1,8 +1,8 @@
 package org.example.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alipay.api.response.AlipayTradePagePayResponse;
 import org.example.utils.Pay;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +16,10 @@ import java.util.Objects;
  * @date 2023/5/24 17:15:42
  */
 @RestController
-public class UserController {
-    @RequestMapping("/test")
-    public String getNameById(String id){
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/pay")
+    public String pay(){
         AlipayTradePagePayResponse result =Pay.pay();
         if(Objects.nonNull(result) && result.isSuccess()){
             System.out.println("调用成功");
@@ -26,6 +27,5 @@ public class UserController {
         }
         System.out.println("调用失败");
         return "error";
-
     }
 }
